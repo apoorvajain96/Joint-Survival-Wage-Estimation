727c71a173c5311c2e2bc5b365720556faa93d65
Apoorva <apoorvajain96@gmail.com>
2015-09-14 00:03:11 -0400

    Saving parameter values at every iteration in fminsearch

---
+++ joint.m
@@ -0,0 +1,19 @@
+function [p fval history] = joint(x0, lw_r, y, ysm, idind, X, E, age, mig_age)
+func = overall_joint_estimation_cost_function(lw_r, y, ysm, idind, X, E, age, mig_age);
+      history = [];
+    options = optimset('OutputFcn', @myoutput);
+    [p fval] = fminsearch(func, x0, options);
+        
+    function stop = myoutput(p,optimvalues,state);
+        stop = false;
+        if ~isequal(state,'iterrupt')                   
+            if size(history,1) >=20
+                history = history(2:20,:); 
+            end
+          history = [history; p'];
+          history
+          save('param_history', 'history', '-ascii')
+        end
+   end
+
+end
